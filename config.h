f0ce08e9d74e2bfa22cc247b30ea56d09a40fdb7
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-11-23 22:53:47 +0530

    wallpaper script keybind added

--- config.h
+++ config.h
@@ -110,6 +110,7 @@ static Key keys[] = {
 
 	/* Firefox spawn keybind */
 	{ MODKEY,             		XK_b,      spawn,      	   {.v = firefox } },
+	{ MODKEY,             		XK_w,      spawn,      	   SHCMD("$HOME/wallpaper.sh") },
 	{ MODKEY|ControlMask|ShiftMask, XK_q,      spawn,      	   SHCMD("$HOME/powermenu.sh") },
 
 	TAGKEYS(                        XK_1,                      0)
